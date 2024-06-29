0835c0c23f3c1287160e1b565b2335f3779ef54d
sangram-sethi <sangramsethi28@gmail.com>
2024-06-29 11:25:10 +0530

    bug fixes in homepage, changed background image link, added dynamic color text.

--- static/javascripts/quote.js
+++ static/javascripts/quote.js
@@ -1,13 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://api.quotable.io/random")
-    .then((response) => response.json())
-    .then((data) => {
-      const quoteContainer = document.getElementById("quoteContainer");
-      quoteContainer.innerHTML = `
-                <p class="quote text-white text-center">
-                    "${data.content}"
-                </p>
-                <p class="author text-white text-center">- <em>${data.author}</em></p>`;
-    })
-    .catch((error) => console.error("Error fetching the quote:", error));
+  // Function to fetch and display a random quote
+  function fetchQuote() {
+      fetch("https://api.quotable.io/random")
+          .then(response => response.json())
+          .then(data => {
+              const quoteContainer = document.getElementById("quoteContainer");
+              quoteContainer.innerHTML = `
+                  <p class="quote text-white text-center">
+                      "${data.content}"
+                  </p>
+                  <p class="author text-white text-center">- <em>${data.author}</em></p>`;
+          })
+          .catch(error => console.error("Error fetching the quote:", error));
+  }
+
+  // Call fetchQuote function to display initial quote
+  fetchQuote();
+
+  // Function to determine if a color is dark
+  function isDarkColor(color) {
+      const rgb = color.match(/\d+/g);
+      const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+      return brightness < 128;
+  }
+
+  const body = document.body;
+  const computedColor = window.getComputedStyle(body).backgroundColor;
+
+  // Check if background color is dark and apply appropriate text color
+  if (isDarkColor(computedColor)) {
+      body.classList.add('dark-text');
+  }
 });
+
